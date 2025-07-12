a8962a4bb9153117824e888b4a55ab884ecd1fcc
Apoorva <apoorvashankar611@gmail.com>
2025-07-12 16:22:31 +0530

    Update settings.py

--- rewear/rewear_project/settings.py
+++ rewear/rewear_project/settings.py
@@ -7,7 +7,7 @@ For more information on this file,
 https://docs.djangoproject.com/en/5.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/5.2/ref/settings/
+https://docs.djangoproject.com/en/5.2/ref/settings/    
 """
 
 from pathlib import Path
