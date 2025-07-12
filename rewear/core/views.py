c7cf0818c2d4744ccc6032329be40719c7d606a5
CharanTej1474 <charantej1474@gmail.com>
2025-07-12 16:41:02 +0530

    Update views.py

--- rewear/core/views.py
+++ rewear/core/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from django.db.models import Q # For complex queries
+from django.db.models import Q 
 
 from .models import ClothingItem, SwapRequest
 from .forms import ClothingItemForm, CustomUserCreationForm
